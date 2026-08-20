f2d465491777fe95e6915c37acfcfe37ed8f6e31
ManasC478 <manas.chougule@gmail.com>
2026-08-19 19:13:18 -0700

    Added session filter by player

--- app/sessions/page.tsx
+++ app/sessions/page.tsx
@@ -1,11 +1,29 @@
 import SessionList from "@/components/session-list"
 import { getSessions } from "@/lib/data"
 import Page from "@/components/page"
+import { FilterCondition } from "@/lib/types"
+import { sessionByPlayerQueryFn } from "@/lib/db/filter/query"
 
 export const dynamic = "force-dynamic"
 
-export default async function SessionsPage() {
-  const sessions = await getSessions()
+export default async function SessionsPage({ searchParams }: {
+  searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+  const query = await searchParams
+  const conditions = []
+  if (query.playerId !== undefined) {
+    const playerId = Number.parseInt(query.playerId)
+    const condition: FilterCondition<number, Set<number>> = {
+      field: 'player_id',
+      type: 'query',
+      value: playerId,
+      query: sessionByPlayerQueryFn
+    }
+    conditions.push(condition)
+  }
+
+  const sessions = await getSessions(conditions)
+  sessions.sort((a, b) => b.date.localeCompare(a.date))
 
   return (
     <Page>
--- lib/db/filter/query.ts
+++ lib/db/filter/query.ts
@@ -7,3 +7,14 @@ export const hasTagQueryFn: QueryFn<string[], Set<number>> = async (tags) => {
   if (!data) return new Set();
   return new Set(data.map(r => r.session_id));
 };
+
+export const sessionByPlayerQueryFn: QueryFn<number, Set<number>> = async (playerId) => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from('buy_ins')
+    .select('session_id')
+    .eq('player_id', playerId);
+
+  if (error) return new Set();
+  return new Set(data.map(r => r.session_id));
+}
